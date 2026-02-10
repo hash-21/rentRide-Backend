eccdcf085a6ce72f6a085c68dd08f69bfa1baa12
hash-21 <64836065+hash-21@users.noreply.github.com>
2026-02-10 17:27:12 +0530

    made changes

--- rental/settings.py
+++ rental/settings.py
@@ -143,6 +143,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS settings
 CORS_ALLOWED_ORIGINS = [
+    "https://rent-ride-frontend.vercel.app/",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
